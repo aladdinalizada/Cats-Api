828b84ed1d72d48f638a2455bc8e5e7baef4ca98
aladdinalizada <tu7c5zneb@code.edu.az>
2022-12-13 05:03:50 +0400

    fix problem

--- script.js
+++ script.js
@@ -8,20 +8,21 @@ const getCatData = async () => {
 };
 getCatData().then((data) => {
   data.forEach((elem) => {
-    // console.log(elem.image.url);
-    // console.log(elem.name);
-    // console.log(elem.description);
-    // console.log(elem.wikipedia_url);
+    // elem.description = elem.description.substring(0, 100);
     row.innerHTML += `
     <div class="col-4">
         <div class="card" style="width: 18rem">
-          <img class="card-img-top" src="${elem.image.url}" alt="Card image cap" />
+          <img class="card-img-top" src="${
+            elem.image.url
+          }" alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">${elem.name}</h5>
             <p class="card-text">
-              ${elem.description}
+              ${elem.description.substring(0, 200)}
             </p>
-            <a href="${elem.wikipedia_url}" class="btn btn-primary">Go somewhere</a>
+            <a href="${
+              elem.wikipedia_url
+            }" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
       </div>
